4dfbf83ac72e60b3e1f61ea6ef68aa187b2de443
Danielum225 <danielalvarezpalmeiro@gmail.com>
2022-09-01 18:54:35 +0000

    prototipo

--- src/front/js/pages/inicio-sesion.js
+++ src/front/js/pages/inicio-sesion.js
@@ -3,5 +3,51 @@ import "../../styles/inicio-sesion.css";
 import { Link } from "react-router-dom";
 
 export const Inicio_sesion = () => {
-  return <></>;
+  return (
+    <>
+      <div className="row">
+        <div className="col-6">
+          <form className="bg-white px-2 py-2">
+            <div className="mb-3">
+              <label for="exampleInputEmail1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              <div id="emailHelp" className="form-text">
+                We'll never share your email with anyone else.
+              </div>
+            </div>
+            <div className="mb-3">
+              <label for="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
+            <div className="mb-3 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+              />
+              <label className="form-check-label" for="exampleCheck1">
+                Check me out
+              </label>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
