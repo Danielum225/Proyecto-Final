5d947e7ccd56afce96365ec0e42ae6c5623c37f4
Danielum225 <danielalvarezpalmeiro@gmail.com>
2022-09-10 16:34:36 +0000

    cb3

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -10,13 +10,47 @@ export const Home = () => {
           <h1 className="navbar-brand ms-2">Pethome</h1>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active text-dark" aria-current="page">
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle text-dark"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
                   Perros
                 </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item text-dark">Comida</a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item text-dark">Juguetes</a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item text-dark">Higiene</a>
+                  </li>
+                </ul>
               </li>
-              <li className="nav-item">
-                <a className="nav-link text-dark">Gatos</a>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle text-dark"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Gatos
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item text-dark">Comida</a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item text-dark">Juguetes</a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item text-dark">Higiene</a>
+                  </li>
+                </ul>
               </li>
               <li className="nav-item dropdown">
                 <a
@@ -29,23 +63,36 @@ export const Home = () => {
                 </a>
                 <ul className="dropdown-menu">
                   <li>
-                    <a className="dropdown-item text-dark">Action</a>
+                    <a className="dropdown-item text-dark">Comida</a>
                   </li>
                   <li>
-                    <a className="dropdown-item text-dark">Another action</a>
+                    <a className="dropdown-item text-dark">Juguetes</a>
                   </li>
                   <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item text-dark">
-                      Something else here
-                    </a>
+                    <a className="dropdown-item text-dark">Higiene</a>
                   </li>
                 </ul>
               </li>
-              <li className="nav-item">
-                <a className="nav-link text-dark">Reptiles</a>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle text-dark"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Reptiles
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item text-dark">Comida</a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item text-dark">Juguetes</a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item text-dark">Higiene</a>
+                  </li>
+                </ul>
               </li>
             </ul>
             <Link to="/inicio-sesion">
