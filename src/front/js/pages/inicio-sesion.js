addc0058c8dfcde23e724a48c8a7ae913f5dfec6
Danielum225 <danielalvarezpalmeiro@gmail.com>
2022-09-11 18:18:58 +0000

    cambios responsive

--- src/front/js/pages/inicio-sesion.js
+++ src/front/js/pages/inicio-sesion.js
@@ -1,8 +1,17 @@
 import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
 import "../../styles/inicio-sesion.css";
 import { Link } from "react-router-dom";
 
 export const Inicio_sesion = () => {
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [contraseña, setContraseña] = useState("");
+
+  const handleSubmit = () => {
+    actions.login(email, contraseña);
+  };
+
   return (
     <>
       <div className="container-fluid">
@@ -16,8 +25,11 @@ export const Inicio_sesion = () => {
       </div>
       <div className="container">
         <div className="row mt-5">
-          <div className="col-6 mx-auto">
-            <form className="bg-white rounded-1 px-3 py-3">
+          <div className="col-12 col-md-6 mx-auto">
+            <form
+              onSubmit={handleSubmit}
+              className="bg-white rounded-1 px-3 py-3"
+            >
               <div className="row">
                 <div className="col-5 mx-auto">
                   <h3 className="mx-auto">Iniciar sesión</h3>
@@ -31,6 +43,9 @@ export const Inicio_sesion = () => {
                   type="email"
                   className="form-control"
                   id="exampleInputEmail1"
+                  onChange={(event) => {
+                    setEmail(event.target.value);
+                  }}
                   aria-describedby="emailHelp"
                 />
               </div>
@@ -42,6 +57,9 @@ export const Inicio_sesion = () => {
                   type="password"
                   className="form-control"
                   id="exampleInputPassword1"
+                  onChange={(event) => {
+                    setContraseña(event.target.value);
+                  }}
                 />
               </div>
               <button type="submit" className="btn btn-primary">
